b25583705ec574c2b4d718bbeb4b5262b8fb657d
YourUsername <you@example.com>
2024-11-21 19:40:43 +0300

    inventory wip

--- Assets/Scripts/Ui/UiContextMenu.cs
+++ Assets/Scripts/Ui/UiContextMenu.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UiContextMenu : MonoBehaviour
 {
@@ -16,19 +18,35 @@ public class UiContextMenu : MonoBehaviour
         _container = (RectTransform)transform;
     }
 
-    void Start()
+    private void Clear()
     {
-        int amount = 5;
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            GameObject child = transform.GetChild(i).gameObject;
+            if (child != _buttonTemplate.gameObject) Destroy(child.gameObject);
+        }
+    }
+
+    public void GenerateButtons(Action[] profile, GameObject pickable)
+    {
+        Clear();
+
         float buttonHeight = _buttonTemplate.sizeDelta.y;
 
-        _container.sizeDelta = new Vector3(_container.sizeDelta.x, _padding * 2 + (buttonHeight + _padding / 2) * amount);
+        _container.sizeDelta = new Vector3(_container.sizeDelta.x, _padding * 2 + (buttonHeight + _padding / 2) * profile.Length);
 
-        for (int i = 0; i < amount; i++)
+        for (int i = 0; i < profile.Length; i++)
         {
+            Action action = profile[i];
+
             RectTransform buttonTransform = Instantiate(_buttonTemplate, transform);
             buttonTransform.gameObject.SetActive(true);
 
-            buttonTransform.anchoredPosition = new Vector2(buttonTransform.anchoredPosition.x, -(_padding + buttonHeight / 2 + (buttonHeight + _padding / 2) * i) );
+            buttonTransform.anchoredPosition = new Vector2(buttonTransform.anchoredPosition.x, -(_padding + buttonHeight / 2 + (buttonHeight + _padding / 2) * i));
+
+            Button button = buttonTransform.GetComponent<Button>();
+            button.GetComponentInChildren<TMP_Text>().text = profile[i].Name;
+            button.onClick.AddListener(() => action.Activate(pickable));
         }
     }
 }
